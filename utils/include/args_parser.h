41dcaa49027f6c47173d4d396408e88141fe6b43
Reversive <matiaspavan@gmail.com>
2022-05-28 17:39:00 -0300

    feat: added POSIX args context data structure

--- utils/include/args_parser.h
+++ utils/include/args_parser.h
@@ -1,4 +1,20 @@
 #ifndef _ARGS_PARSER_H
 #define _ARGS_PARSER_H
 
+typedef enum {EMBED = 0, EXTRACT} ACTION;
+typedef enum {LSB1 = 0, LSB4, LSBI} STEG_MODE;
+typedef enum {AES128 = 0, AES192, AES256, DES} ALGO_MODE;
+typedef enum {ECB = 0, CFB, OFB, CBC} BLOCK_MODE;
+
+typedef struct steg_configuration_t {
+	ACTION action;
+	char *in_file_path;
+	char *bmp_carrier_path;
+	char *bmp_out_path;
+	STEG_MODE steg_mode;
+	ALGO_MODE algo_mode;
+	BLOCK_MODE block_mode;
+	char *enc_password;
+} steg_configuration; 
+
 #endif
