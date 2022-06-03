2f75eaa9fc020d60e888df32ac06066fdcd2c810
Reversive <matiaspavan@gmail.com>
2022-06-03 13:02:23 -0300

    feat: fixed embed/extract flags

--- utils/args_parser.c
+++ utils/args_parser.c
@@ -1,12 +1,12 @@
 #include "include/args_parser.h"
 
-
 static int embed_flag, extract_flag;
 static char *steg_options[3] = {"LSB1", "LSB4", "LSBI"};
 static char *algo_options[4] = {"AES128", "AES192", "AES256", "DES"};
 static char *block_options[4] = {"ECB", "CFB", "OFB", "CBC"};
 
-int iequals(const char* a, const char* b){
+int iequals(const char *a, const char *b)
+{
     unsigned int size1 = strlen(a);
     if (strlen(b) != size1)
         return 0;
@@ -17,99 +17,115 @@ int iequals(const char* a, const char* b){
 }
 
 static struct option long_opts[] =
-{
-	{"embed", no_argument, &embed_flag, 0},
-	{"extract", no_argument, &extract_flag, 0},
-	{"in", required_argument, NULL, 'i'},
-	{"p", required_argument, NULL, 'p'},
-	{"out", required_argument, NULL, 'o'},
-	{"steg", required_argument, NULL, 's'},
-	{"a", required_argument, NULL, 'a'},
-	{"m", required_argument, NULL, 'm'},
-	{"pass", required_argument, NULL, 'j'},
-	{0, 0, 0, 0}
-};
+    {
+        {"embed", no_argument, &embed_flag, 1},
+        {"extract", no_argument, &extract_flag, 1},
+        {"in", required_argument, NULL, 'i'},
+        {"p", required_argument, NULL, 'p'},
+        {"out", required_argument, NULL, 'o'},
+        {"steg", required_argument, NULL, 's'},
+        {"a", required_argument, NULL, 'a'},
+        {"m", required_argument, NULL, 'm'},
+        {"pass", required_argument, NULL, 'j'},
+        {0, 0, 0, 0}};
 
 steg_configuration_ptr init_steg_config()
 {
-	steg_configuration_ptr steg_config = malloc(sizeof(steg_configuration));
-	if(steg_config == NULL)
-	{
-		// TO-DO: Logging
-		exit(-1);
-	}
-	steg_config->action 			= NO_ACTION;
-	steg_config->in_file_path 		= NULL;
-	steg_config->bmp_carrier_path 	= NULL;
-	steg_config->bmp_out_path 		= NULL;
-	steg_config->steg_mode 			= NO_STEG;
-	steg_config->algo_mode 			= NO_ALGO;
-	steg_config->block_mode 		= NO_BLOCK;
-	steg_config->enc_password 		= NULL;
-	return steg_config;
+    steg_configuration_ptr steg_config = malloc(sizeof(steg_configuration));
+    if (steg_config == NULL)
+    {
+        // TO-DO: Logging
+        exit(-1);
+    }
+    steg_config->action = NO_ACTION;
+    steg_config->in_file_path = NULL;
+    steg_config->bmp_carrier_path = NULL;
+    steg_config->bmp_out_path = NULL;
+    steg_config->steg_mode = NO_STEG;
+    steg_config->algo_mode = NO_ALGO;
+    steg_config->block_mode = NO_BLOCK;
+    steg_config->enc_password = NULL;
+    return steg_config;
 }
 
 int parse_string_arg(char *input, char **options, size_t size, int default_value)
 {
-	for(int i = 0; i < size; i++)
-	{
-		if(iequals(input, options[i]))
-		{
-			return i;
-		}
-	}
-	return default_value;
+    for (int i = 0; i < size; i++)
+    {
+        if (iequals(input, options[i]))
+        {
+            return i;
+        }
+    }
+    return default_value;
 }
 
 steg_configuration_ptr parse_options(
-	int argc, 
-	char *argv[]
-) 
+    int argc,
+    char *argv[])
 {
-	steg_configuration_ptr steg_config = init_steg_config();
-	int option;
-	while((option = getopt_long_only(argc, argv, "eri:p:o:s:a:m:j:", long_opts, NULL)) != -1)
-	{
-		switch (option)
-		{
-		case 0:
-			// Flag cases.
-			break;
-		case 'i':
-			steg_config->in_file_path = optarg;
-			break;
-		case 'p':
-			steg_config->bmp_carrier_path = optarg;
-			break;
-		case 'o':
-			steg_config->bmp_out_path = optarg;
-			break;
-		case 's':
-			steg_config->steg_mode = parse_string_arg(optarg, steg_options, 3, NO_STEG);
-			break;
-		case 'a':
-			steg_config->algo_mode = parse_string_arg(optarg, algo_options, 4, NO_ALGO);
-			break;
-		case 'm':
-			steg_config->block_mode = parse_string_arg(optarg, block_options, 4, NO_BLOCK);
-			break;
-		case 'j':
-			steg_config->enc_password = optarg;
-			printf ("option -pass (-j) with value `%s'\n", optarg);
-			break;
-		default:
-			fprintf(stderr, "Invalid argument options\n");
-			exit(-1);
-			break;
-		}
-	}
+    steg_configuration_ptr steg_config = init_steg_config();
+    int option;
+    while ((option = getopt_long_only(argc, argv, "eri:p:o:s:a:m:j:", long_opts, NULL)) != -1)
+    {
+        switch (option)
+        {
+        case 0:
+            // Flag cases, ignore.
+            break;
+        case 'i':
+            steg_config->in_file_path = optarg;
+            break;
+        case 'p':
+            steg_config->bmp_carrier_path = optarg;
+            break;
+        case 'o':
+            steg_config->bmp_out_path = optarg;
+            break;
+        case 's':
+            steg_config->steg_mode = parse_string_arg(optarg, steg_options, 3, NO_STEG);
+            break;
+        case 'a':
+            steg_config->algo_mode = parse_string_arg(optarg, algo_options, 4, NO_ALGO);
+            break;
+        case 'm':
+            steg_config->block_mode = parse_string_arg(optarg, block_options, 4, NO_BLOCK);
+            break;
+        case 'j':
+            steg_config->enc_password = optarg;
+            break;
+        default:
+            fprintf(stderr, "Invalid argument option.\n");
+            exit(-1);
+            break;
+        }
+    }
 
-	if(embed_flag && extract_flag)
-	{
-		printf("Can't use extract and embed at the same time, please use one or the other.\n");
-		free(steg_config);
-		exit(-1);
-	}
+    if (embed_flag && extract_flag)
+    {
+        printf("Can't use extract and embed at the same time, please use either one or the other.\n");
+        free(steg_config);
+        exit(-1);
+    }
 
-	return steg_config;
+    if (embed_flag &&
+        (steg_config->in_file_path == NULL ||
+         steg_config->bmp_carrier_path == NULL ||
+         steg_config->bmp_out_path == NULL ||
+         steg_config->steg_mode == NO_STEG))
+    {
+        printf("Missing arguments for embedding, make sure to provide -in <file> -p <bitmapfile> -out <bitmapfile> -steg <LSB1 | LSB4 | LSBI>\n");
+        exit(-1);
+    }
+
+    if (extract_flag &&
+        (steg_config->bmp_carrier_path == NULL ||
+         steg_config->bmp_out_path == NULL ||
+         steg_config->steg_mode == NO_STEG))
+    {
+        printf("Missing arguments for extracting, make sure to provide -p <bitmapfile> -out <bitmapfile> -steg <LSB1 | LSB4 | LSBI>\n");
+        exit(-1);
+    }
+
+    return steg_config;
 }
--- utils/include/args_parser.h
+++ utils/include/args_parser.h
@@ -6,28 +6,62 @@
 #include <ctype.h>
 #include <getopt.h>
 
-typedef enum {NO_ACTION = -1, EMBED, EXTRACT} ACTION;
-typedef enum {NO_STEG = -1, LSB1, LSB4, LSBI} STEG_MODE;
-typedef enum {NO_ALGO = -1, AES128, AES192, AES256, DES} ALGO_MODE;
-typedef enum {NO_BLOCK = -1, ECB, CFB, OFB, CBC} BLOCK_MODE;
+enum ERROR_CODES
+{
+    STATUS_SUCCESS,
+    STATUS_ERROR
+};
 
-typedef struct steg_configuration_t {
-	ACTION action;
-	char *in_file_path;
-	char *bmp_carrier_path;
-	char *bmp_out_path;
-	STEG_MODE steg_mode;
-	ALGO_MODE algo_mode;
-	BLOCK_MODE block_mode;
-	char *enc_password;
-} steg_configuration; 
+typedef enum
+{
+    NO_ACTION = -1,
+    EMBED,
+    EXTRACT
+} ACTION;
 
-typedef steg_configuration * steg_configuration_ptr;
+typedef enum
+{
+    NO_STEG = -1,
+    LSB1,
+    LSB4,
+    LSBI
+} STEG_MODE;
+
+typedef enum
+{
+    NO_ALGO = -1,
+    AES128,
+    AES192,
+    AES256,
+    DES
+} ALGO_MODE;
+
+typedef enum
+{
+    NO_BLOCK = -1,
+    ECB,
+    CFB,
+    OFB,
+    CBC
+} BLOCK_MODE;
+
+typedef struct steg_configuration_t
+{
+    ACTION action;
+    char *in_file_path;
+    char *bmp_carrier_path;
+    char *bmp_out_path;
+    STEG_MODE steg_mode;
+    ALGO_MODE algo_mode;
+    BLOCK_MODE block_mode;
+    char *enc_password;
+} steg_configuration;
+
+typedef steg_configuration *steg_configuration_ptr;
 
 extern steg_configuration_ptr steg_config;
 
 steg_configuration_ptr init_steg_config();
 steg_configuration_ptr parse_options(int argc, char *argv[]);
-
 
 #endif
