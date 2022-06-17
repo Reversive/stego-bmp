47ddbecdbebe5cb805643562bb15100af6f8e982
Reversive <matiaspavan@gmail.com>
2022-06-17 15:51:20 -0300

    refactor: removed global variables

--- entry_point.c
+++ entry_point.c
@@ -5,54 +5,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-FILE *carrier_fptr;
-steg_configuration_ptr steg_config;
-bitmap_metadata_ptr bmp_metadata;
-
-/*
- * Function: sig_handler
- * ----------------------------
- *   Handler for SIGTERM & SIGINT signals.
- *   signal: signal number.
- */
-static void sig_handler(const int signal);
-
 int main(
     int argc,
     char *argv[])
 {
-    signal(SIGTERM, sig_handler);
-    signal(SIGINT, sig_handler);
+    steg_configuration_ptr steg_config = parse_options(argc, argv);
 
-    steg_config = parse_options(argc, argv);
-
-    // Carrier is mandatory for any option
-    carrier_fptr = fopen(steg_config->bmp_carrier_path, "rw");
+    FILE *carrier_fptr = fopen(steg_config->bmp_carrier_path, "rw");
     if (carrier_fptr == NULL)
     {
         logw(ERROR, "%s\n", "Invalid carrier file path.");
-        exit_clean_up(STATUS_ERROR);
+        exit_clean_up(STATUS_ERROR, NULL, steg_config, NULL);
     }
 
-    // Parse bitmap
-    bmp_metadata = bitmap_read_metadata(carrier_fptr);
+    bitmap_metadata_ptr bmp_metadata = bitmap_read_metadata(carrier_fptr);
     if (bmp_metadata == NULL)
     {
         logw(ERROR, "%s\n", "Couldn't parse bmp carrier.");
-        exit_clean_up(STATUS_ERROR);
+        exit_clean_up(STATUS_ERROR, carrier_fptr, steg_config, NULL);
     }
 
     password_data p_data = {0};
     int should_encrypt = steg_config->enc_password != NULL;
-
-    // Init encryption data
     if (should_encrypt)
     {
         p_data.password = steg_config->enc_password;
         if (-1 == init_password_data(&p_data, steg_config->algo_mode, steg_config->block_mode))
         {
             logw(ERROR, "%s\n", "Couldn't init password data.");
-            exit_clean_up(STATUS_ERROR);
+            exit_clean_up(STATUS_ERROR, carrier_fptr, steg_config, bmp_metadata);
         }
     }
 
@@ -63,18 +44,18 @@ int main(
         payload = generate_payload(steg_config, &payload_size, p_data, should_encrypt);
         if (payload == NULL)
         {
-            exit_clean_up(ERROR);
+            exit_clean_up(ERROR, carrier_fptr, steg_config, bmp_metadata);
         }
         logw(DEBUG, "%s\n", "Hiding payload into meta");
         if (-1 == hide_payload_into_meta(steg_config->steg_mode, payload, bmp_metadata, payload_size))
         {
             logw(ERROR, "%s", "Error hiding payload");
-            exit_clean_up(STATUS_ERROR);
+            exit_clean_up(STATUS_ERROR, carrier_fptr, steg_config, bmp_metadata);
         }
         if (-1 == metadata_to_file(bmp_metadata, steg_config->bmp_out_path))
         {
             logw(ERROR, "Error dumping meta in file %s", steg_config->bmp_out_path);
-            exit_clean_up(STATUS_ERROR);
+            exit_clean_up(STATUS_ERROR, carrier_fptr, steg_config, bmp_metadata);
         }
     }
     else
@@ -85,7 +66,7 @@ int main(
         if (-1 == unload_payload(extracted_payload, &p_data, should_encrypt, steg_config->bmp_out_path))
         {
             logw(ERROR, "Error unloading payload in %s", steg_config->bmp_out_path);
-            exit_clean_up(STATUS_ERROR);
+            exit_clean_up(STATUS_ERROR, carrier_fptr, steg_config, bmp_metadata);
         }
         free(extracted_payload);
     }
@@ -98,16 +79,14 @@ int main(
     {
         clear_password_data(&p_data);
     }
-    exit_clean_up(STATUS_SUCCESS);
+    exit_clean_up(STATUS_SUCCESS, carrier_fptr, steg_config, bmp_metadata);
 }
 
-static void sig_handler(const int signal)
-{
-    logw(DEBUG, "signal %d, cleaning up and exiting\n", signal);
-    exit_clean_up(EXIT_SUCCESS);
-}
-
-void exit_clean_up(int err_code)
+void exit_clean_up(
+    int err_code,
+    FILE *carrier_fptr,
+    steg_configuration_ptr steg_config,
+    bitmap_metadata_ptr bmp_metadata)
 {
     if (steg_config != NULL)
     {
--- include/entry_point.h
+++ include/entry_point.h
@@ -10,7 +10,10 @@
  *   Cleans up the memory allocated by the program and exits.
  *
  *   err_code: error code.
+ *   carrier_fptr: file pointer to carrier file.
+ *   steg_config: stegnography configuration.
+ *   bmp_metadata: bitmap metadata.
  */
-void exit_clean_up(int err_code);
+void exit_clean_up(int err_code, FILE *carrier_fptr, steg_configuration_ptr steg_config, bitmap_metadata_ptr bmp_metadata);
 
 #endif
